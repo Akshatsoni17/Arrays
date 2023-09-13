ef11d3f4342bbb29d391ebe07b8640c042f16ec8
Akshatsoni17 <141300277+Akshatsoni17@users.noreply.github.com>
2023-09-13 11:43:31 +0530

    Add files via upload

---
+++ declarationarr.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int test[6] = {9,4,6,8,0,12};
+    int i,check;
+    bool x = 0;
+    int smst;
+    //cout<<"Enter the element to check in array: "<<endl;
+    //cin>>check;
+   // for(i=0;i<6;i++)
+   // {
+    //    if(check==test[i])
+    //    {
+     //       cout<<"It exists in this array"<<endl;
+     //       x=1;
+     //       cout<<"It is indexed at: "<<i<<endl;
+     //   }
+   // }
+    //if(x==0)
+   // {
+    //     cout<<"It does not exist in this array";
+    //}
+    smst = test[0];
+    for(i=1;i<6;i++)
+    {
+        //if(smst>test[i])
+        //{
+          //  smst = test[i];
+           //cout<<"The smallest element is: "<<smst<<endl;
+        //}
+        if(smst<test[i])
+        {
+            smst = test[i];
+           //cout<<"The smallest element is: "<<smst<<endl;
+        }
+        
+    }
+    cout<<"The largest element is: "<<smst<<endl;
+}
